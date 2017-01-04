975c5b6c6beca8d1f4d7d3466709721b0e462d7b
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-01-04 12:32:31 -0500

    worked on command line arguments

--- MealTracker.java
+++ MealTracker.java
@@ -1,12 +1,25 @@
 public class MealTracker{
+    
+    public static void main(String[]args){ //either prompt them to add one by one or force them to enter all arguments
+	String date;
+	String meal;
+	String food;
+	int cal;
+	int protein;
+	int carb;
+	int fat;
+	if (args.length == 2){
+	    System.out.println("2");
+	}
+	else if (args.length == 3){
+	    System.out.println("3");
+	}
+	else{
+	    System.out.println("Usage: java MealTracker [date]");
+	}
+    }
 
-
-
-
-
-
-
-
+    
 
 
 
