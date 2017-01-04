4c25f52811d50ecba36cbcffffce0ec92273aed1
Amanda Neelapriyantha <aneelapriyantha@gmail.com>
2017-01-04 12:32:21 -0500

    worked on adding meals to the csv file

--- MealTracker.java
+++ MealTracker.java
@@ -1,13 +1,14 @@
 public class MealTracker{
-
-
-
-
-
-
-
-
-
-
-
+    public static void enter(String mealType, String meal){
+	FileWriter fw = new FileWriter("Meals.csv", true);
+	if(mealType.equals("Breakfast")){
+	    fw.append(date);
+	}
+	//String info = nutritionalInfo(mealType);
+	fw.append(mealType, meal, "info");
+	fw.close();
+	}
+    public static void main(String[]args){
+	enter("Breakfast", "Cereal");
+    }
 }
