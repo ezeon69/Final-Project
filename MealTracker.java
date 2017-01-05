0a0b0afded1494f11e30617b2b03156c49257bed
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2017-01-05 03:22:39 -0500

    merge

--- MealTracker.java
+++ MealTracker.java
@@ -1,14 +1,19 @@
 public class MealTracker{
-    
+    String date;
+    String meal;
+    String food;
+    int cal;
+    int protein;
+    int carb;
+    int fat;
+
     public static void main(String[]args){ //either prompt them to add one by one or force them to enter all arguments
-	String date;
-	String meal;
-	String food;
-	int cal;
-	int protein;
-	int carb;
-	int fat;
-	if (args.length == 2){
+	if (args.length == 1){
+	    if (args[0].equals("total")){
+		System.out.println("test");
+	    }
+	}
+	else if (args.length == 2){
 	    System.out.println("2");
 	}
 	else if (args.length == 3){
@@ -19,7 +24,7 @@ public class MealTracker{
 	}
     }
 
-    public static void enter(String mealType, String meal){
+    /*public static void enter(String mealType, String meal){
 	FileWriter fw = new FileWriter("Meals.csv", true);
 	if(mealType.equals("Breakfast")){
 	    fw.append(date);
@@ -30,6 +35,6 @@ public class MealTracker{
 	}
     public static void main(String[]args){
 	enter("Breakfast", "Cereal");
-    }
+	}*/
 
 }
