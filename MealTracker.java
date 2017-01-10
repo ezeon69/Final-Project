2bf8a03dfc7f9a0e87e29fc8f75e689a67df835e
Amanda Neelapriyantha <aneelapriyantha@gmail.com>
2017-01-10 02:44:37 -0500

    Finished demo

--- MealTracker.java
+++ MealTracker.java
@@ -15,28 +15,28 @@ public class MealTracker{
 
     public static void main(String[]args) throws IOException{ //either prompt them to add one by one or force them to enter all arguments
 	Scanner sc = new Scanner(System.in);
-	if(args.length = 0){
+	if(args.length == 0){
 	    System.out.print("Hello, welcome to the Meal Tracker. What is the date of today?");
-	    Data = sc.next();
+	    Date = sc.next();
 	    System.out.println("What is the type of the meal you want to add? (Breakfast, Lunch, or Dinner)");
 	    MealType = sc.next();
 	    System.out.println("What is the meal you want to add?");
-	    Meal = sc.next;
+	    Meal = sc.next();
 	    System.out.println("How many calories are in your meal?");
-	    Cal = args[3];
+	    Cal = sc.next();
 	    System.out.println("How much protein is in your meal?");
-	    Protein = args[4];
+	    Protein = sc.next();
 	    System.out.println("How many grams of carbohydrates are in your meal?");
-	    Carb = args[5];
+	    Carb = sc.next();
 	    System.out.println("How many grams of fat are in your meal?");
-	    Fat = args[6];
+	    Fat = sc.next();
 	    mealAdd(Date, MealType, Meal, Cal, Protein, Carb, Fat);
             System.out.println("Successfully added meal.");
 	}
 	else if (args.length == 2){
 	    Date = args[0];
 	    if(args[1].equals("total")){
-		System.out.println(readFile());
+		readFile();
 	    }
 	}
 	else if (args.length == 3){
@@ -72,14 +72,17 @@ public class MealTracker{
     public static String[][] readFile() throws FileNotFoundException{
 	String text = new Scanner(new File("Meals.csv")).useDelimiter("\\A").next(); //file to string
 	String lines[] = text.split("\n"); //splits string by csv
-    String[][] meals = new String[lines.length][];
-    for (int counter = 0; counter < lines.length; counter++) {
-        meals[counter] = lines[counter].split(",");
+	String[][] meals = new String[lines.length][];
+	for (int counter = 0; counter < lines.length; counter++) {
+	    meals[counter] = lines[counter].split(",");
+	    for(int count = 0; count < meals[counter].length; count ++){
+		System.out.println(meals[counter][count]);
+	    }
+	}
+	return meals;
     }
-    return meals;
-    }
-
-    public String totalCal(String date) throws FileNotFoundException{
+    
+    public static String totalCal(String date) throws FileNotFoundException{
 	int total = 0;
 	int counter = 0;
 	while (counter < readFile().length){
@@ -92,7 +95,7 @@ public class MealTracker{
 	return Integer.toString(total);
     }
 
-    public String totalPro(String date) throws FileNotFoundException{
+    public static String totalPro(String date) throws FileNotFoundException{
 	int total = 0;
 	int counter = 0;
 	while (counter < readFile().length){
@@ -105,12 +108,13 @@ public class MealTracker{
 	return Integer.toString(total);
     }
 
-    public String totalCarb(String date) throws FileNotFoundException{
+    public static String totalCarb(String date) throws FileNotFoundException{
 	int total = 0;
 	int counter = 0;
+	String[][]whatIs = readFile();
 	while (counter < readFile().length){
-	    if ((readFile()[counter][0]) == date){
-		total += Integer.parseInt(readFile()[counter][5]);
+	    if ((whatIs[counter][0]).equals(date)){
+		total += Integer.parseInt(whatIs[counter][5]);
 		counter++;
 	    }
 	    counter++;
@@ -118,7 +122,7 @@ public class MealTracker{
 	return Integer.toString(total);
     }
 
-    public String totalFat(String date) throws FileNotFoundException{
+    public static String totalFat(String date) throws FileNotFoundException{
 	int total = 0;
 	int counter = 0;
 	while (counter < readFile().length){
